522b22f96c454bf90e31f87032b51f57a8b65b52
dennis310797 <dennis0201@gmail.com>
2021-06-07 22:15:50 -0300

    Inserting protection through header

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from types import MethodDescriptorType
 from flask import Flask, request
 import flask
 from bs4 import BeautifulSoup
-from pymongo.message import update
+from pymongo import message
+from pymongo.message import insert, update
 from selenium import webdriver
 from time import sleep
 import json
@@ -17,15 +18,14 @@ options.add_argument("--headless")
 cliente = MongoClient("mongodb://localhost:27017/")
 db = cliente.myFirstmongoDB
 conexao = db.testeUser
+autho = db.authorization
 #print(conexao.find_one())
 #conexao.insert({"nome":"joaquim","idade": 21})
 
 app = Flask("Bot")
 
 def openDriver(uf=None):
-
     url="https://www.4devs.com.br/gerador_de_pessoas"
-
     page = webdriver.Chrome("./chromedriver", chrome_options=options)
     page.get(url)
 
@@ -53,47 +53,20 @@ def openDriver(uf=None):
     list_filter_address = ["cep","endereco","numero","bairro","cidade","estado"]
     data["address"]={key:data.pop(key,None) for key in list_filter_address}
     
-    
-
     page.quit()
 
     return data
 
 
-"""
-    sleep(1)
-    page.find_element_by_xpath("/html/body/main/div/div[2]/div/div[4]/div[1]/div[3]/label/input").click()
-    sleep(1)
-    page.find_element_by_xpath("/html/body/main/div/div[2]/div/div[4]/div[1]/div[7]/div/textarea")
-    #page.find_element_by_xpath('//*[@id="cep_estado"]').send_keys("SC")
-
-
-
-
-
-
-caso nao funcione a conexao com o mongo:
-    sudo mongod --dbpath /var/lib/mongodb/ --journal
-    mongo <- para abrir
-"""
-
-
-
 def generateResponses (status, message, name_content=False,content=False):
     response = {}
-    print(content)
     response["status"] = status
     response["message"]= message
 
     if name_content and content:
         response[name_content] = content
 
-    print("-"*50,'\n',response)
-    print("-"*50,'\n'*5)
     return response
-
-
-
 
 
 def insertData(data):
@@ -102,7 +75,6 @@ def insertData(data):
 def findInput(cpf):
     if len(cpf) == 11:
         new_cpf=f"{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}"
-
         cpf=new_cpf
     
     data=conexao.find_one({"personal_information.cpf":cpf})
@@ -110,26 +82,18 @@ def findInput(cpf):
     data["id"]=str(data["_id"])
     del data["_id"]
 
-    
-
     return data
-
 
 
 def updateUser(body,cpf):
     user=findInput(str(cpf))
-    
-    id_client=str(user["id"])
     del user["id"]
+
     for i in body:
-        
         for key in user:
             for j in user[key]:
-                if i == j:
-                    #conexao.find_one_and_update(dict(key.j),to_data)
-                    print("ok")
+                if i == j:  
                     to_data={"$set":{key+'.'+i:body[i]}}
-                    
                     conexao.find_one_and_update(
                         {
                             'personal_information.cpf':cpf
@@ -137,14 +101,33 @@ def updateUser(body,cpf):
                         to_data
                     )
 
-
     return findInput(str(cpf))
+
+def read_authorizathion(header=None,methods=None):
+    #Methods -> 'insert' 'delete' 'update' 'read'
+    data = autho.find_one({'key':header})
+
+    if not data:
+        message="User Not Authorized"
+        status=404
+    
+    else:
+        if data['authorization'][methods]:
+            message="Authorized"
+            status=200
+    
+        else:
+            message="Only authorized people"
+            status=401
+    
+    return status,message
 
 
 #FAMOSO OLÁ MUNDO
 @app.route("/olamundo", methods=["GET"])
 def olamundo ():
     header = request.headers.get('Authorization')
+    
     if '88a00cb' == header:
         return {"Message":"Consegui"}
     else:
@@ -152,54 +135,69 @@ def olamundo ():
         resp = flask.Response("Foo bar baz")
         resp.headers['Acces-Control-Allow-Origin'] = '*'
 
-        return resp
+        return {"Mensagem":"VOCE NAO TEM AUTORIZAÇÃO"}
 
 
 #INSERINDO DADOS NO BANCO DE DADOS
 @app.route("/input/users", methods=["GET"])
 def inputBots():
-
-    data=openDriver()
-
-    #Função para salvar dados no mongo    
-    insertData(data.copy())
-
-
-    return generateResponses (200, "Usuario cadastrado com sucesso", "User", data)
+    header=request.headers.get("Authorization")
+    status,message=read_authorizathion(header,'insert')
     
+    if status == 200:
+        data=openDriver()  
+        insertData(data.copy())
+        return generateResponses(200, "Usuario cadastrado com sucesso", "User", data)
+    
+    else:
+        return generateResponses(status,message,'Authorization',header)
 
 @app.route("/status/input/especifica/<cpf>", methods=["GET"])
 def find_one_input(cpf):
-
-    data=findInput(str(cpf))
-
+    header=request.headers.get("Authorization")
+    status,message=read_authorizathion(header,'read')
     
-
-    return generateResponses(200,"Usuario encontrado com sucesso", "User", data)
+    if status == 200:
+        data=findInput(str(cpf))
+        return generateResponses(200,"Usuario encontrado com sucesso", "User", data)
+    
+    else:
+        return generateResponses(status,message,'Authorization',header)
 
 
 @app.route("/update/input/<cpf>", methods=["POST"])
 def update_user(cpf):
     body = request.get_json()
+    header=request.headers.get("Authorization")
+    status,message=read_authorizathion(header,'update')
     
-    data=updateUser(body,cpf)
-
-    return generateResponses(200,"Usuario Atualizado com sucesso", "User", data)
+    if status == 200:
+        data=updateUser(body,cpf)
+        return generateResponses(200,"Usuario Atualizado com sucesso", "User", data)
+    
+    else:
+        return generateResponses(status,message,'Authorization',header)
 
 @app.route("/del/one_user/<cpf>", methods=["GET"])
 def del_user(cpf):
-    data=conexao.find_one_and_delete({"personal_information.cpf":cpf})
-    del data["_id"]
-    return generateResponses(200,"Usuario Deletado com sucesso", "User", data)
+    header=request.headers.get("Authorization")
+    status,message=read_authorizathion(header,'delete')
+    
+    if status == 200:
+        data=conexao.find_one_and_delete({"personal_information.cpf":cpf})
+        del data["_id"]
+        return generateResponses(200,"Usuario Deletado com sucesso", "User", data)
 
-
+    else:
+        return generateResponses(status,message,'Authorization',header)
 
 
 app.run()
 """
 if __name__ == '__main__':
-    #TESTAR OS DADOS QUE É CAPTURADO PELO BOT
-#    data_main = openDriver()
+    pass
+    TESTAR OS DADOS QUE É CAPTURADO PELO BOT
+    data_main = openDriver()
     cpf = 61559414910
     teste=findInput(str(cpf))
 """
