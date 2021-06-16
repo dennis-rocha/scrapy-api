2863d5091f9e732bf6037e1fbdbc5274e7b9babd
dennis310797 <dennis0201@gmail.com>
2021-06-15 21:39:02 -0300

    Adding more filters and fixing CPF

--- app.py
+++ app.py
@@ -7,26 +7,42 @@ from bs4 import BeautifulSoup
 from pymongo import message
 from pymongo.message import insert, update
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
+from datetime import date,datetime
 import json
 from pymongo import MongoClient
 from werkzeug.datastructures import Authorization
 #Ocultar Webdriver
-options = webdriver.ChromeOptions()
-options.add_argument("--headless")
+_options = webdriver.ChromeOptions()
+_options.add_argument("--headless")
+_webdriver = ChromeDriverManager().install() 
 #conexao mongo
 cliente = MongoClient("mongodb://localhost:27017/")
 db = cliente.myFirstmongoDB
 conexao = db.testeUser
 autho = db.authorization
-#print(conexao.find_one())
+today = date.today()
+#(conexao.find_one())
 #conexao.insert({"nome":"joaquim","idade": 21})
 
 app = Flask("Bot")
 
+def ageCalc(data):
+    try:
+        born = data['data_nasc']
+    except:
+        born = data['personal_information']['data_nasc']
+
+    _date = datetime.strptime(born, '%Y-%m-%d').date()
+
+    return  today.year - _date.year -((today.month, today.day) < (_date.month, _date.day))
+
 def openDriver(uf=None):
     url="https://www.4devs.com.br/gerador_de_pessoas"
-    page = webdriver.Chrome("./chromedriver", chrome_options=options)
+    page=webdriver.Chrome(_webdriver, options=_options )
+
+    #page = webdriver.Chrome("./chromedriver", chrome_options=options)
     page.get(url)
 
     #if uf:
@@ -41,6 +57,15 @@ def openDriver(uf=None):
     data=json.loads(html.find('textarea').text)
 
     #FILTRANDO OS DADOS
+    a=data['cpf']
+    data['cpf']=f'{a[:3]}{a[4:7]}{a[8:11]}{a[12:]}'
+
+    day,month,year=data['data_nasc'].split('/')
+    data['data_nasc']=f"{year}-{month}-{day}"
+
+    data['idade'] = ageCalc(data)
+
+
     list_filter=["pai","senha","signo","peso", "altura","tipo_sanguineo","cor"]
     [data.pop(key,None) for key in list_filter]
 
@@ -73,11 +98,9 @@ def insertData(data):
     conexao.insert_one(data)
 
 def findInput(cpf):
-    if len(cpf) == 11:
-        new_cpf=f"{cpf[0:3]}.{cpf[3:6]}.{cpf[6:9]}-{cpf[9:11]}"
-        cpf=new_cpf
     
-    data=conexao.find_one({"personal_information.cpf":cpf})
+    conexao.find_one_and_update({'personal_information.cpf':cpf},{"$set":{'personal_information.idade':ageCalc(conexao.find_one({"personal_information.cpf":cpf}))}})
+    data = conexao.find_one({"personal_information.cpf":cpf})    
 
     data["id"]=str(data["_id"])
     del data["_id"]
@@ -88,6 +111,8 @@ def findInput(cpf):
 def updateUser(body,cpf):
     user=findInput(str(cpf))
     del user["id"]
+
+    conexao.find_one_and_update({'personal_information.cpf':cpf},{"$set":{'personal_information.idade':ageCalc(user)}})
 
     for i in body:
         for key in user:
@@ -147,7 +172,7 @@ def inputBots():
     if status == 200:
         data=openDriver()  
         insertData(data.copy())
-        return generateResponses(200, "Usuario cadastrado com sucesso", "User", data)
+        return generateResponses(200, "Usuario cadastrado com sucesso", "user", data)
     
     else:
         return generateResponses(status,message,'Authorization',header)
@@ -159,7 +184,7 @@ def find_one_input(cpf):
     
     if status == 200:
         data=findInput(str(cpf))
-        return generateResponses(200,"Usuario encontrado com sucesso", "User", data)
+        return generateResponses(200,"Usuario encontrado com sucesso", "user", data)
     
     else:
         return generateResponses(status,message,'Authorization',header)
@@ -173,7 +198,7 @@ def update_user(cpf):
     
     if status == 200:
         data=updateUser(body,cpf)
-        return generateResponses(200,"Usuario Atualizado com sucesso", "User", data)
+        return generateResponses(200,"Usuario Atualizado com sucesso", "user", data)
     
     else:
         return generateResponses(status,message,'Authorization',header)
@@ -186,7 +211,7 @@ def del_user(cpf):
     if status == 200:
         data=conexao.find_one_and_delete({"personal_information.cpf":cpf})
         del data["_id"]
-        return generateResponses(200,"Usuario Deletado com sucesso", "User", data)
+        return generateResponses(200,"Usuario Deletado com sucesso", "user", data)
 
     else:
         return generateResponses(status,message,'Authorization',header)
@@ -196,8 +221,9 @@ app.run()
 """
 if __name__ == '__main__':
     pass
+    
     TESTAR OS DADOS QUE É CAPTURADO PELO BOT
     data_main = openDriver()
     cpf = 61559414910
     teste=findInput(str(cpf))
-"""
+    """
